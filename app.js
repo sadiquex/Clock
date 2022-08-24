4c167916b757c103f08f6389cc175ca0a76ef9cb
sadiquex <abubakasaddik1@gmail.com>
2022-08-24 22:03:42 +0000

    added clock functionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const hourHand = document.querySelector(".hour");
 const minuteHand = document.querySelector(".minute");
 const secondHand = document.querySelector(".second");
-const time = document.querySelector(".time");
+const timeElement = document.querySelector(".time");
 const date = document.querySelector(".date");
 const centerPoint = document.querySelector(".center-point");
 const toggle = document.querySelector(".toggle");
@@ -32,26 +32,50 @@ const months = [
 ];
 
 // toggle dark mode
-toggle.addEventListener('click', (e) => {
-    const html = document.querySelector('html')
-    if(html.classList.contains('dark')) {
-        html.classList.remove('dark')
-        e.target.innerHTML = 'Dark Mode';
-    } else {
-                html.classList.add("dark");
-                e.target.innerHTML = "Light Mode";
-    }
-})
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark Mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light Mode";
+  }
+});
 
 // clock functionality
-function setTime () {
-    const time = new Date();
-    const month = time.getMonth();
-    const day = time.getDay();
-    const hours = time.getHours();
-    const minutes = time.getMinutes();
-    const seconds = time.getSeconds();
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
 
-
+  hourHand.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)})deg`;
+  minuteHand.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)})deg`;
+  secondHand.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)})deg`;
 }
+
+// function scale(number, inMin, inMax, outMin, outMax) {
+//   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+// }
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+
+
+// number - hand on clock
+// inMin -  (timeMin)
+// inMax -  (timeMax)
+// outMin -  (clockMin)
+// outMax -  (clockMax)
+
+
 setTime();
+
+// keep calling it for it to keep running
+setInterval(setTime, 1000)
